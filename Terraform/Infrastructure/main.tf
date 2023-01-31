c06c00cd52a59fc7f693e25038f136141e05f4ba
Keith Patton <keith.patton@outlook.com>
2023-02-01 08:43:15 +1300

    moved acr to basic tier to save $$

--- Terraform/Infrastructure/main.tf
+++ Terraform/Infrastructure/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_container_registry" "default" {
   name                = "${local.acr_name}"
   resource_group_name = azurerm_resource_group.default.name
   location            = azurerm_resource_group.default.location
-  sku                 = "Standard"
+  sku                 = "Basic"
 }
 
 ### AKS Cluster
