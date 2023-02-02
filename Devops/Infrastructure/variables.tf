9087aa3e2d2290d1978a9f1c01176d5a3f8de4c1
Keith Patton <keith.patton@outlook.com>
2023-02-02 17:57:42 +1300

    fix tf var quotes

--- Devops/Infrastructure/variables.tf
+++ Devops/Infrastructure/variables.tf
@@ -1,61 +1,61 @@
 ﻿variable "rg_name" {
   type        = string
   description = "The name of the resource group to be used to provision core Azure resources."
-  default = #{rgName}#
+  default = "#{rgName}#"
 }
 
 variable "rg_aks_nodes_name" {
   type        = string
   description = "The name of the resource group to be used to provision AKS Node resources."
-  default = #{rgAksNodesName}#
+  default = "#{rgAksNodesName}#"
 }
 
 variable "location" {
   type        = string
   description = "The location to create azure resources within."
-  default = #{location}#
+  default = "#{location}#"
 }
 
 variable "acr_name" { 
   description = "The name of the Azure Container Registry instance."
   type        = string
-  default = #{acrName}#
+  default = "#{acrName}#"
 }
 
 variable "aks_name" { 
   description = "The name of the AKS instance."
   type        = string
-  default = #{aksName}#
+  default = "#{aksName}#"
 }
 
 variable "aks_workload_identity_name" { 
   description = "The name of the AKS workload identity."
   type        = string
-  default = #{aksWorkloadIdentityName}#
+  default = "#{aksWorkloadIdentityName}#"
 }
 
 variable "aks_federated_identity_name" { 
   description = "The name of the AKS federated identity."
   type        = string
-  default = #{aksFederatedIdentityName}#
+  default = "#{aksFederatedIdentityName}#"
 }
 
 variable "aks_namespace" { 
   description = "The default namespace to be set on AKS."
   type        = string
-  default = #{aksNamespace}#
+  default = "#{aksNamespace}#"
 }
 
 variable "aks_workload_identity_service_account_name" { 
   description = "The name of the service account to use on AKS."
   type        = string
-  default = #{aksWorkloadIdentityServiceAccountName}#
+  default = "#{aksWorkloadIdentityServiceAccountName}#"
 }
 
 variable "aks_vm_size" {
   description = "Kubernetes VM size."
   type        = string
-  default = #{aksVmSize}#
+  default = "#{aksVmSize}#"
 }
 
 variable "aks_node_count" {
@@ -67,5 +67,5 @@ variable "aks_node_count" {
 variable "kv_name" { 
   description = "The name of the Key Vault instance."
   type        = string
-  default = #{kvName}#
+  default = "#{kvName}#"
 }
