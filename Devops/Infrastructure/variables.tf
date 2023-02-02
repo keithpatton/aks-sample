72e602115b10557d0d98b610b9dcdbd31318493e
Keith Patton <keith.patton@outlook.com>
2023-02-02 17:53:48 +1300

    allow token replacement in tf variables

--- Devops/Infrastructure/variables.tf
+++ Devops/Infrastructure/variables.tf
@@ -1,59 +1,71 @@
 ﻿variable "rg_name" {
   type        = string
   description = "The name of the resource group to be used to provision core Azure resources."
+  default = #{rgName}#
 }
 
 variable "rg_aks_nodes_name" {
   type        = string
   description = "The name of the resource group to be used to provision AKS Node resources."
+  default = #{rgAksNodesName}#
 }
 
 variable "location" {
   type        = string
   description = "The location to create azure resources within."
+  default = #{location}#
 }
 
 variable "acr_name" { 
   description = "The name of the Azure Container Registry instance."
   type        = string
+  default = #{acrName}#
 }
 
 variable "aks_name" { 
   description = "The name of the AKS instance."
   type        = string
+  default = #{aksName}#
 }
 
 variable "aks_workload_identity_name" { 
   description = "The name of the AKS workload identity."
   type        = string
+  default = #{aksWorkloadIdentityName}#
 }
 
 variable "aks_federated_identity_name" { 
   description = "The name of the AKS federated identity."
   type        = string
+  default = #{aksFederatedIdentityName}#
 }
 
 variable "aks_namespace" { 
   description = "The default namespace to be set on AKS."
   type        = string
+  default = #{aksNamespace}#
 }
 
 variable "aks_workload_identity_service_account_name" { 
   description = "The name of the service account to use on AKS."
   type        = string
+  default = #{aksWorkloadIdentityServiceAccountName}#
 }
 
 variable "aks_vm_size" {
   description = "Kubernetes VM size."
   type        = string
+  default = #{aksVmSize}#
 }
 
 variable "aks_node_count" {
   description = "Number of nodes to deploy for Kubernetes"
   type        = number
+  default = #{aksNodeCount}#
 }
 
 variable "kv_name" { 
   description = "The name of the Key Vault instance."
   type        = string
+  default = #{kvName}#
 }
