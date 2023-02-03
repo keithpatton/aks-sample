f6f1a02f42468771a6a07a9656815b821937b16f
Keith Patton <keith.patton@outlook.com>
2023-02-04 11:30:06 +1300

    tf fix

--- Devops/Infra/Terraform/variables.tf
+++ Devops/Infra/Terraform/variables.tf
@@ -1,125 +1,125 @@
 ﻿variable "tenants" {
-  type = list(string)
+  type        = list(string)
   description = "Tenant List"
-  default = #{tenantsJson}#
+  default     = #{tenantsJson}#
 }
 
 variable "rg_name" {
   type        = string
   description = "The name of the resource group to be used to provision core Azure resources."
-  default = "#{rgName}#"
+  default     = "#{rgName}#"
 }
 
 variable "rg_aks_nodes_name" {
   type        = string
   description = "The name of the resource group to be used to provision AKS Node resources."
-  default = "#{rgAksNodesName}#"
+  default     = "#{rgAksNodesName}#"
 }
 
 variable "location" {
   type        = string
   description = "The location to create azure resources within."
-  default = "#{location}#"
+  default     = "#{location}#"
 }
 
 variable "acr_name" { 
   description = "The name of the Azure Container Registry instance."
   type        = string
-  default = "#{acrName}#"
+  default     = "#{acrName}#"
 }
 
 variable "aks_name" { 
   description = "The name of the AKS instance."
   type        = string
-  default = "#{aksName}#"
+  default     = "#{aksName}#"
 }
 
 variable "aks_workload_identity_name" { 
   description = "The name of the AKS workload identity."
   type        = string
-  default = "#{aksWorkloadIdentityName}#"
+  default     = "#{aksWorkloadIdentityName}#"
 }
 
 variable "aks_federated_identity_name" { 
   description = "The name of the AKS federated identity."
   type        = string
-  default = "#{aksFederatedIdentityName}#"
+  default     = "#{aksFederatedIdentityName}#"
 }
 
 variable "aks_namespace" { 
   description = "The default namespace to be set on AKS."
   type        = string
-  default = "#{aksNamespace}#"
+  default     = "#{aksNamespace}#"
 }
 
 variable "aks_workload_identity_service_account_name" { 
   description = "The name of the service account to use on AKS."
   type        = string
-  default = "#{aksWorkloadIdentityServiceAccountName}#"
+  default     = "#{aksWorkloadIdentityServiceAccountName}#"
 }
 
 variable "aks_vm_size" {
   description = "Kubernetes VM size."
   type        = string
-  default = "#{aksVmSize}#"
+  default     = "#{aksVmSize}#"
 }
 
 variable "aks_node_count" {
   description = "Number of nodes to deploy for Kubernetes"
   type        = number
-  default = #{aksNodeCount}#
+  default     = #{aksNodeCount}#
 }
 
 variable "kv_name" { 
   description = "The name of the Key Vault instance."
   type        = string
-  default = "#{kvName}#"
+  default     = "#{kvName}#"
 }
 
 variable "sql_server_name" { 
   description = "The name of the Azure SQL Server."
   type        = string
-  default = "#{sqlServerName}#"
+  default     = "#{sqlServerName}#"
 }
 
 variable "sql_elasticpool_name" { 
   description = "The name of the Azure SQL Elastic Pool."
   type        = string
-  default = "#{sqlElasticPoolName}#"
+  default     = "#{sqlElasticPoolName}#"
 }
 
 variable "sql_elasticpool_sku_name" { 
   description = "The sku name of the Azure SQL Elastic Pool."
   type        = string
-  default = "#{sqlElasticPoolSkuName}#"
+  default     = "#{sqlElasticPoolSkuName}#"
 }
 
 variable "sql_elasticpool_sku_tier" { 
   description = "The sku tier of the Azure SQL Elastic Pool."
   type        = string
-  default = "#{sqlElasticPoolSkuTeir}#"
+  default     = "#{sqlElasticPoolSkuTeir}#"
 }
 
 variable "sql_elasticpool_sku_family" { 
   description = "The sku family of the Azure SQL Elastic Pool."
   type        = string
-  default = "#{sqlElasticPoolSkuFamily}#"
+  default     = "#{sqlElasticPoolSkuFamily}#"
 }
 
 variable "sql_elasticpool_sku_capacity" { 
   description = "The sku capacity of the Azure SQL Elastic Pool."
-  type        = integer
-  default = #{sqlElasticPoolSkuCapacity}#
+  type        = number
+  default     = #{sqlElasticPoolSkuCapacity}#
 }
 
 variable "sql_admin_username" { 
   description = "The username of the Sql Administrator."
   type        = string
-  default = "#{sqlAdminUsername}#"
+  default     = "#{sqlAdminUsername}#"
 }
 
 variable "sql_ad_admin_username" { 
   description = "The username of the Azure AD Sql Administrator."
   type        = string
-  default = "#{sqlAdAdminUsername}#"
+  default     = "#{sqlAdAdminUsername}#"
 }
