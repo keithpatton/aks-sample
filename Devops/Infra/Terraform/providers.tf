b2309fae0f4f6978870af09e574fd762da85e88f
Keith Patton <keith.patton@outlook.com>
2023-02-04 12:13:49 +1300

    tf update

--- Devops/Infra/Terraform/providers.tf
+++ Devops/Infra/Terraform/providers.tf
@@ -38,4 +38,5 @@ provider "azurerm" {
 # Configure the Azure Active Directory Provider
 provider "azuread" {
   tenant_id = data.azurerm_client_config.current.tenant_id
+  subscription_id = data.azurerm_client_config.current.subscription_id
 }
