80d3081a6061de620d255c3a29a9ce209c290514
Keith Patton <keith.patton@outlook.com>
2023-02-07 14:02:29 +1300

    swagger deploy

--- AksWorkloadIdentitySample.Api/Program.cs
+++ AksWorkloadIdentitySample.Api/Program.cs
@@ -16,11 +16,11 @@ namespace AksWorkloadIdentitySample.Api
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
