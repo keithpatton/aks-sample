698cf84a241104eaa0b62716994963123a580305
Keith Patton <keith.patton@outlook.com>
2023-02-15 09:19:05 +1300

    Job init

---
+++ AksSample.Job/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AksSample.Job
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tenant = Environment.GetEnvironmentVariable("TENANT");
+            var waitFor = Environment.GetEnvironmentVariable("WAIT_FOR");
+            Console.WriteLine($"Running job for {tenant}, waiting for {waitFor}ms");
+            Console.WriteLine("2 + 2...let me think...");
+            Thread.Sleep(Convert.ToInt32(waitFor));
+            Console.WriteLine("The answer is.... 4!");
+        }
+    }
+}
