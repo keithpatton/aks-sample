603888ac9837a9e408b585f66c7b7cca93244be1
Keith Patton <keith.patton@outlook.com>
2023-02-15 13:53:40 +1300

    fix space

--- Devops/App/Terraform/main.tf
+++ Devops/App/Terraform/main.tf
@@ -57,8 +57,8 @@ resource "azurerm_federated_identity_credential" "aks" {
   issuer                = data.azurerm_kubernetes_cluster.aks.oidc_issuer_url
   subject               = <<EOF
   ${each.value == var.aks_workload_identity_name_default_suffix ? 
-    "system:serviceaccount:${var.aks_namespace_default}:${var.aks_workload_identity_service_account_name}" : 
-    "system:serviceaccount:${var.aks_namespace_default}-${each.value}:${var.aks_workload_identity_service_account_name}"
+  "system:serviceaccount:${var.aks_namespace_default}:${var.aks_workload_identity_service_account_name}" : 
+  "system:serviceaccount:${var.aks_namespace_default}-${each.value}:${var.aks_workload_identity_service_account_name}"
   }
   EOF
 }
